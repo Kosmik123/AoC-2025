d12867e95d8f22218dd872fb415da98890004a76
Kosmik <malpa.w.klapkach@gmail.com>
2026-01-15 00:15:38 +0100

    Day 2 (part 2)

--- day2.py
+++ day2.py
@@ -2,29 +2,33 @@ from helpers import load_input
 
 def is_id_valid(id) -> bool:
     digits = str(id)
-    if len(digits) % 2 != 0:
-        return True
-
     halfway_index = len(digits) // 2
-    left = digits[:halfway_index]
-    right = digits[halfway_index:]
-    return right != left     
+    for checked_length in range(1, halfway_index + 1):
+        if len(digits) % checked_length != 0:
+            continue
+
+        checked_sequence = digits[0:checked_length]
+        repeat_count = len(digits) // checked_length
+        repeated_sequence = repeat_count * checked_sequence
+        if repeated_sequence == digits:
+            return False
+
+    return True
 
 
-def append_invalid_ids(start, end, invalid_ids_list: list) -> int:
-    invalid_ids_count = 0
+def append_invalid_ids(start, end, invalid_ids_list: list):
     for id in range(start, end + 1):
         if not is_id_valid(id):
             invalid_ids_list.append(id)
 
-    return invalid_ids_count
+
 
 input = '''11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
 824824821-824824827,2121212118-2121212124'''
 
-
 input = load_input()
+
 ranges = input.split(',')
 all_invalid_ids = []
 for id_range in ranges:
