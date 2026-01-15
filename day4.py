e72006a8679954b69251f4eae57eb0c066169329
Kosmik <malpa.w.klapkach@gmail.com>
2026-01-15 14:31:06 +0100

    Day 4.2

--- day4.py
+++ day4.py
@@ -6,7 +6,10 @@ class Room:
         lines = input.split('\n')
         self.width = len(lines[0])
         self.height = len(lines)
-        self.data = ''.join(lines)
+        self.data = []
+        for line in lines:
+            for ch in line:
+                self.data.append(ch)
 
     def is_inside(self, x: int, y: int) -> bool:
         return x >= 0 and y >= 0 and x < self.width and y < self.height
@@ -19,7 +22,12 @@ class Room:
     def is_occupied(self, x: int, y: int) -> bool:
         character = self.get_character(x, y)
         return character == '@'
-    
+
+    def set_occupied(self, x: int, y: int, occupy: bool = True):
+        assert self.is_inside(x, y)
+        self.data[y * self.width + x] = '@' if occupy else '.'
+
+
 
 def count_neighbours(room: Room, x: int, y: int) -> int:
     count = 0
@@ -51,13 +59,25 @@ input = '''..@@.@@@@.
 
 input = load_input()
 
-accessible_rolls_count = 0
+total_accessible_rolls_count = 0
 room = Room(input)
-for y in range(room.width):
-    for x in range(room.height):
-        if room.is_occupied(x, y):
-            neighbours_count = count_neighbours(room, x, y)
-            if neighbours_count < 4:
-                accessible_rolls_count += 1
+while True:
+    accessible_positions = []
+    detected_rolls_count = 0
+    for y in range(room.width):
+        for x in range(room.height):
+            if room.is_occupied(x, y):
+                neighbours_count = count_neighbours(room, x, y)
+                if neighbours_count < 4:
+                    detected_rolls_count += 1
+                    accessible_positions.append((x, y))
 
-print(accessible_rolls_count)
+    for x, y in accessible_positions:
+        room.set_occupied(x, y, False)
+
+    print(detected_rolls_count)
+    total_accessible_rolls_count += detected_rolls_count
+    if detected_rolls_count == 0:
+        break
+
+print(total_accessible_rolls_count)
