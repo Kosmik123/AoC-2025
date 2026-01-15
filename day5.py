4899ff6ad870ff713cade7ac9160df203ac00496
Kosmik <malpa.w.klapkach@gmail.com>
2026-01-15 16:36:50 +0100

    Day 5.2

--- day5.py
+++ day5.py
@@ -1,13 +1,38 @@
 from helpers import load_input
 
+
+class SingleRange:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+
+    def can_join(self, other: SingleRange) -> bool:
+        return self.start <= other.start <= self.end + 1 or other.start <= self.start <= other.end + 1 
+
+    def join(self, other: SingleRange) -> None:
+        self.start = min(self.start, other.start)
+        self.end = max(self.end, other.end)
+
+    def __str__(self):
+        return f"({self.start}, {self.end})"
+
+
 class IdRanges:
     def __init__(self):
-        self.ranges = []
+        self.ranges: list[SingleRange] = []
         self.max_id = -1
         self.min_id = None
 
     def add_range(self, start: int, end: int):
-        self.ranges.append((start,end))
+        new_range = SingleRange(start, end)
+        for i in range(len(self.ranges) - 1, -1, -1):
+            other = self.ranges[i]
+            if new_range.can_join(other):
+                self.ranges.pop(i)
+                new_range.join(other)
+
+        self.ranges.append(new_range)
+        
         if end > self.max_id:
             self.max_id = end
         if self.min_id == None or start < self.min_id:
@@ -18,8 +43,8 @@ class IdRanges:
         self.add_range(int(start), int(end))
 
     def __contains__(self, key: int) -> bool:
-        for start, end in self.ranges:
-            if start <= key <= end:
+        for r in self.ranges:
+            if r.start <= key <= r.end:
                 return True
         
         return False
@@ -62,10 +87,12 @@ input = load_input()
 
 
 fresh, ingredients = parse_input(input)
-fresh_count = 0
-for id in range(fresh.min_id, fresh.max_id + 1):
-    if id in fresh:
-        print(id, "is fresh")
-        fresh_count += 1
 
-print(fresh_count)
+
+all_fresh_count = 0
+for single_range in fresh.ranges:
+    count = single_range.end - single_range.start + 1 
+    print(count)
+    all_fresh_count += count
+
+print(all_fresh_count)
