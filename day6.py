eea112a34e70134612acf2ac581e987f68b9964c
Kosmik <malpa.w.klapkach@gmail.com>
2026-01-15 18:18:18 +0100

    Day 6.2

--- day6.py
+++ day6.py
@@ -41,6 +41,7 @@ def parse_numbers_line(line: str):
         num = int(str_numbers[col].strip())
         problems[col].numbers.append(num)
 
+
 def parse_operations_line(line: str):
     global problems    
     operations = [d for d in line.split(' ') if len(d) > 0]
@@ -48,6 +49,19 @@ def parse_operations_line(line: str):
         problems[col].operation = operations[col].strip()
 
 
+def make_number(input: list[str], col: int) -> int:
+    result = 0
+    for row in input:
+        if col >= len(row):
+            continue
+        ch = row[col]
+        if ch.isdigit():
+            result *= 10
+            result += int(ch)
+    return result
+    
+def operation_at_column(input: list[str], col: int) -> str:
+    return input[-1][col]
 
 input = '''123 328  51 64 
  45 64  387 23 
@@ -57,17 +71,34 @@ input = '''123 328  51 64
 input = load_input()
 
 lines = input.split('\n')
+operation_columns: list[int] = []
+column_count = len(lines[0])
+for col in range(column_count):
+    if col >= len(lines[-1]):
+        break
+    char = lines[-1][col]
+    if char != ' ':
+        operation_columns.append(col)
 
-for i in range(len(lines) - 1):
-    line = lines[i]
-    parse_numbers_line(line)
+print (operation_columns)
 
-parse_operations_line(lines[-1])
+for i in range(len(operation_columns)):
+    start_col = operation_columns[i]
+    end_col = (operation_columns[i + 1] - 1 if (i + 1) < len(operation_columns) else column_count) 
+    new_problem = Problem()
+    new_problem.operation = operation_at_column(lines, start_col)
+    for col in range(start_col, end_col):
+        num = make_number(lines, col)
+        new_problem.numbers.append(num)
 
+    problems.append(new_problem)
 
 grand_total = 0
 for p in problems:
-    answer = p.calculate()
-    grand_total += answer
+    print(p)
+    result = p.calculate()
+    grand_total += result
 
 print(grand_total)
+
+
