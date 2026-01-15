e87a4e3266ff7334ae61ced930ee0004d60b1211
Kosmik <malpa.w.klapkach@gmail.com>
2026-01-15 13:39:39 +0100

    Day 3.2

--- day3.py
+++ day3.py
@@ -1,14 +1,13 @@
 from helpers import load_input
 
 
-def get_max_digit(bank: str, start: int, end: int) -> tuple[int, int]:
-    
+def get_max_digit(bank: str, start: int, end: int) -> tuple[int, int]:    
     assert 0 <= start < len(bank)
-    assert start < end <= len(bank)
+    assert start <= end < len(bank)
     
     max_value = 0
     max_value_index = None
-    for index in range(start, end):
+    for index in range(start, end + 1):
         value = int(bank[index])
         if max_value_index == None or value > max_value:
             max_value = value
@@ -17,11 +16,19 @@ def get_max_digit(bank: str, start: int, end: int) -> tuple[int, int]:
     return max_value, max_value_index        
 
 
-def get_max_joltage(bank: str) -> int:
-    assert len(bank) > 1
-    first, first_index = get_max_digit(bank, 0, len(bank) - 1)
-    second, second_index = get_max_digit(bank, first_index + 1, len(bank))
-    return int(str(first) + str(second))
+def get_max_joltage(bank: str, requested_count: int = 2) -> int:
+    assert len(bank) >= requested_count
+
+    bateries = []
+    previous_digit_index = -1
+    for index in range(requested_count):
+        remaining_count = requested_count - len(bateries)
+        last_possible_position_in_bank = len(bank) - remaining_count
+        value, index_in_bank = get_max_digit(bank, previous_digit_index + 1, last_possible_position_in_bank)
+        bateries.append(str(value))
+        previous_digit_index = index_in_bank
+
+    return int(''.join(bateries))
 
 
 input = '''987654321111111
@@ -34,7 +41,7 @@ input = load_input()
 lines = input.split('\n')
 total_joltage = 0
 for line in lines:
-    joltage = get_max_joltage(line)
+    joltage = get_max_joltage(line, 12)
     total_joltage += joltage
 
 print(total_joltage)
